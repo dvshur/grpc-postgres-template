d4e88a4374427a3e483145082c594b8251c6f1b5
Dmitry Shuranov <dvshur@gmail.com>
2020-05-06 10:17:23 +0300

    renaming modules

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,30 @@
 mod config;
-mod notifier;
+mod error;
 mod proto;
-mod status;
+mod service;
 mod util;
 
-use notifier::NotifierImpl;
 use proto::push::notifier_server::NotifierServer;
+use service::NotifierImpl;
 use tonic::transport::Server;
 use util::pg_url;
 
 use sqlx::postgres::PgPool;
 
+// todo proper error handling with custom (or derived) error types
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = crate::config::load()?;
 
     let addr = format!("0.0.0.0:{}", config.port).parse()?;
 
-    let pool = PgPool::builder()
+    let _pool = PgPool::builder()
         .max_size(config.pg.pool_size)
         .build(&pg_url(&config.pg))
         .await?;
 
-    // it is possible to customize headers using lower level workaround
-    // see hyper_warp example, as suggested in discord
+    // It is possible to customize error details header using lower level workaround.
+    // See hyper_warp example, as suggested in discord.
     Server::builder()
         .add_service(NotifierServer::new(NotifierImpl::default()))
         .serve(addr)
